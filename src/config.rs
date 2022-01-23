416ae69dbf84967e1ea84ed17265a214146c358f
kaidokert <kaidokert@gmail.com>
2022-01-22 20:30:02 -0800

    Make constants public

--- src/config.rs
+++ src/config.rs
@@ -4,19 +4,19 @@
 //! https://github.com/ryankurte/libsts2lp/blob/master/S2LP_Library/Src/S2LP_Radio.c
 //! Copyright 2018 Ryan Kurte
 
-const MAX_PA_VALUE: usize = 14;
+pub const MAX_PA_VALUE: usize = 14;
 const MIN_PA_VALUE: usize = 31;
 
 /// VCO center frequency in Hz
 const VCO_CENTER_FREQ: usize = 3600000000;
 
 /// Band select factor for high band. Factor B in the equation 2
-const HIGH_BAND_FACTOR: usize = 4;
+pub const HIGH_BAND_FACTOR: usize = 4;
 /// Band select factor for middle band. Factor B in the equation 2
-const MIDDLE_BAND_FACTOR: usize = 8;
+pub const MIDDLE_BAND_FACTOR: usize = 8;
 
 /// Lower limit of the high band: 860 MHz (S2-LPQTR)
-const HIGH_BAND_LOWER_LIMIT: usize = 825900000;
+pub const HIGH_BAND_LOWER_LIMIT: usize = 825900000;
 /// Upper limit of the high band: 940 MHz (S2-LPCBQTR)  
 const HIGH_BAND_UPPER_LIMIT: usize = 1056000000;
 /// Lower limit of the middle band: 430 MHz (S2-LPQTR)  
